696d46023ced020fc2ca506418739e37fdf08d07
Yura Lysenko <yuralysenko1@gmail.com>
2022-07-28 18:01:59 +0300

    Updates

---
+++ static/js/main.3868ec30.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpreact_sum=this.webpackJsonpreact_sum||[]).push([[0],{9:function(t,c,e){"use strict";e.r(c);e(1);var n=e(3),s=e.n(n),a=e(0),b=function(t){var c=t.a,e=void 0===c?0:c,n=t.b,s=void 0===n?0:n;"Sum of ".concat(e," and ").concat(s," is ").concat(e+s)},j=function(){return Object(a.jsxs)(a.Fragment,{children:[Object(a.jsx)(b,{a:2,b:3}),Object(a.jsx)(b,{a:-5,b:0}),Object(a.jsx)(b,{a:-10}),Object(a.jsx)(b,{b:5}),Object(a.jsx)(b,{b:0})]})};s.a.render(Object(a.jsx)(j,{}),document.getElementById("root"))}},[[9,1,2]]]);
+//# sourceMappingURL=main.3868ec30.chunk.js.map
